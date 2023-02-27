34d9cd443ee4caa8e066e4d7e8714ca02908d0c9
Blaser993 <corradodepinto.design@gmail.com>
2023-02-27 16:51:46 +0100

    FEAT stampa mail trmaite api

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 console.log("ciao")
 
-
-
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-      message: 'Hello Vue!'
-    }
-  }
+
+    data() {
+        return {
+        mail: 'ciao',
+        mails: [],
+        }
+    },
+
+    methods: {
+        fetchMails(){
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((res) => {
+            console.log(res.data.response)
+            this.mails.push(res.data.response) 
+        })
+        },
+
+    },
+
+    mounted(){
+        for(let i = 0; i < 10; i++){
+            this.fetchMails()
+        }
+    },
+
+
 }).mount('#app')
+
+
+
+
